be4031a68b1f9ac9eb5048b64d029dfc71053598
IGotAGuy <gibsonsg032@gmail.com>
2017-08-16 15:06:23 -0400

    Initial Push

---
+++ Scripts/script.js
@@ -0,0 +1,24 @@
+﻿/*
+*When updating the javascript, don't forget to change the v value to refresh
+*/
+$(document).ready(function () {
+    //$("#headshotDiv").fadeIn('slow');
+    $(".panel").slideDown('slow');
+    //$(".flip:first").next(".panel").slideDown("slow");
+    //$(".flip").click(function () {
+    //    $(".panel").slideUp('slow');
+    //    if ($(this).next(".panel").is(':visible')) {
+    //        $(this).next(".panel").slideUp("slow");
+    //    }
+    //    else {
+    //        $(this).next(".panel").slideDown("slow");
+    //    }
+    //});
+    $('ul.nav').find('a').click(function () {
+        var $href = $(this).attr('href');
+        var $anchor = $('#' + $href).offset();
+        $('body').animate({ scrollTop: $anchor.top - 10 }, "slow");
+        return false;
+    });
+});
+
